5c08982566a6ec5a72a13a8ba7e68e3c57e9a1e0
Itz-Sangram <113735877+Itz-Sangram@users.noreply.github.com>
2022-09-24 21:01:50 +0530

    Update song.py

--- RocksAlexaRobot/modules/song.py
+++ RocksAlexaRobot/modules/song.py
@@ -64,7 +64,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:15]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n **Made by: @Dr_Asad_Ali** '
+        rep = f'🎙 **Title**: [{title[:15]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n **Made by: @WCFnetwork** '
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -72,7 +72,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('An error Occured! \nReport at @Shayri_Music_Lovers')
+        m.edit('An error Occured! \nReport at @WorldChattingFriendsWCF')
         print(e)
 
     try:
