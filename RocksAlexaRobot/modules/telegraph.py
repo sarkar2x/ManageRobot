f76efe37393cbd83f5fffae470f01b7a610f8f70
TheTeamWCF <113735877+TheTeamWCF@users.noreply.github.com>
2022-10-05 18:03:05 +0530

    Update telegraph.py

--- RocksAlexaRobot/modules/telegraph.py
+++ RocksAlexaRobot/modules/telegraph.py
@@ -16,7 +16,7 @@ from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 
-wibu = "RocksAlexaRobot"
+wibu = "Queen Robot"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=wibu)
 auth_url = r["auth_url"]
