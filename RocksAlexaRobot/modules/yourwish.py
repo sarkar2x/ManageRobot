ddd7a88aef4a5764b391129d9fbaed2f2ec9a785
TheTeamWCF <113735877+TheTeamWCF@users.noreply.github.com>
2022-10-05 16:08:25 +0530

    Update yourwish.py

--- RocksAlexaRobot/modules/yourwish.py
+++ RocksAlexaRobot/modules/yourwish.py
@@ -16,22 +16,22 @@ async def wish(alexa):
          await tbot.send_message(alexa.chat_id, f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol)
    if not alexa.is_reply:
          mm = random.randint(1,100)
-         ALEXA = "https://telegra.ph/file/6760471209d90bcad8b1f.jpg"
+         ALEXA = "https://telegra.ph/file/6497151d4ee17c9e436d4.jpg"
          await tbot.send_file(alexa.chat_id, ALEXA,caption=f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=alexa)
          lol = await alexa.get_reply_message()
          await tbot.send_file(alexa.chat_id, f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol, file=ALEXA)
    if not alexa.is_reply:
          mm = random.randint(1,100)
-         ALEXA = "https://telegra.ph/file/6760471209d90bcad8b1f.jpg"
+         ALEXA = "https://telegra.ph/file/6497151d4ee17c9e436d4.jpg"
          await tbot.send_file(alexa.chat_id,f"**Your wish has been cast.✨**\n\n__chance of success {mm}%__", reply_to=lol, file=ALEXA)
          await tbot.send_file(alexa.chat_id, ALEXA,caption=f"**Your wish has been cast.✨**\n__chance of success {mm}%__", reply_to=alexa)
          lol = await alexa.get_reply_message()
          await tbot.send_file(alexa.chat_id, f"**Your wish has been cast.✨**\n__chance of success {mm}%__", reply_to=lol)
    if not alexa.is_reply:
          mm = random.randint(1,100)
-         ALEXA = "https://telegra.ph/file/6760471209d90bcad8b1f.jpg"
+         ALEXA = "https://telegra.ph/file/6497151d4ee17c9e436d4.jpg"
          await tbot.send_file(alexa.chat_id, ALEXA,caption=f"**Your wish has been cast.✨**\n__chance of success {mm}%__", reply_to=lol,file=alexa)
 
         
    
-        #Trying To Be A 𝗥𝗮𝗶𝗻𝗯𝗼𝘄 In Someone’s 𝗖𝗹𝗼𝘂𝗱.👉 @Give_Me_Heart
+        #Trying To Be A 𝗥𝗮𝗶𝗻𝗯𝗼𝘄 In Someone’s 𝗖𝗹𝗼𝘂𝗱.👉 @LegendDps
